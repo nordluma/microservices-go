b29234fead1dc3c4fa3593ca1d2518565b8794b8
nordluma <maxnordlund.95@hotmail.com>
2025-09-30 22:48:36 +0300

    feat(metadata): setup http server

--- metadata/cmd/main.go
+++ metadata/cmd/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"log"
+	"net/http"
 
-	"google.golang.org/grpc/metadata"
+	"github.com/nordluma/microservices-go/metadata/internal/controller/metadata"
+	httpHandler "github.com/nordluma/microservices-go/metadata/internal/handler/http"
+	"github.com/nordluma/microservices-go/metadata/internal/repository/memory"
 )
 
 func main() {
 	log.Println("Starting movie metadata service")
 	repo := memory.New()
-	ctrl := metadata.New(repo)
+	controller := metadata.New(repo)
+	handler := httpHandler.New(controller)
+
+	http.Handle("/metadata", http.HandlerFunc(handler.GetMetadata))
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal(err)
+	}
 }
